32bb42b6f4294db3f3a1a7d613926247ff8664c9
Ivars Knets <ivars.knets@gmail.com>
2022-04-05 10:47:02 +0300

    Commit 05/04/22
    Some changes, nothing concrete.

--- core/src/main/java/com/adobe/aem/ipp/bikes/core/models/NavigationMenuModel.java
+++ core/src/main/java/com/adobe/aem/ipp/bikes/core/models/NavigationMenuModel.java
@@ -1,35 +1,17 @@
-/*
- *  Copyright 2015 Adobe Systems Incorporated
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package com.adobe.aem.ipp.bikes.core.models;
 
 import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.apache.sling.settings.SlingSettingsService;
 
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
@@ -37,29 +19,27 @@ import com.day.cq.wcm.api.PageManager;
 import java.util.Optional;
 
 @Model(
-    adaptables = Resource.class,
-    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+    adaptables = Resource.class
+    //defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
-public class HelloWorldModel {
+public class NavigationMenuModel {
 
     @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
     @Default(values="No resourceType")
     protected String resourceType;
 
-    @OSGiService
-    private SlingSettingsService settings;
-
     @SlingObject
     private Resource currentResource;
     @SlingObject
     private ResourceResolver resourceResolver;
-
-    private String message;
-
+    /*
     @ValueMapValue
     private String title;
     @ValueMapValue
     private String text;
+*/
+
+    private String message;
 
     @PostConstruct
     protected void init() {
@@ -68,19 +48,20 @@ public class HelloWorldModel {
                 .map(pm -> pm.getContainingPage(currentResource))
                 .map(Page::getPath).orElse("");
 
-        message = "Helloooooooooo World!\n"
-            + "[HW] Resource type is : " + resourceType + "\n"
-            + "[HW] Current page is:  " + currentPagePath + "\n";
+        message = "Hello from Navigation Menu V2!\n"
+            + "[NM] Resource type is: " + resourceType + "\n"
+            + "[NM] Current page is:  " + currentPagePath + "\n";
     }
 
     public String getMessage() {
         return message;
     }
+    /*
     public String getTitle() {
         return StringUtils.isEmpty(title) ? "Hello, this is the default value!" : title;
     }
     public String getText() {
         return StringUtils.isEmpty(text) ? null : text.toUpperCase();
-    }
+    }*/
 
 }
---
+++ ui.frontend/src/main/webpack/components/_navigation-menu.js
@@ -0,0 +1,9 @@
+
+
+$(".cmp-tabs__tab").hover(function() {
+    console.log("Y are u Hoverin?");
+});
+
+$(".cmp-tabs__tab").click(function() {
+    console.log("Y are u Cliking?");
+});
