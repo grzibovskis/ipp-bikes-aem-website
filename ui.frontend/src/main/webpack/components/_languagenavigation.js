d68c543466dd6745aafa3d642910642adc1d1f3b
Verners <97786188+grzibovskis@users.noreply.github.com>
2022-05-12 11:36:26 +0300

    update2

---
+++ ui.frontend/src/main/webpack/components/_languagenavigation.js
@@ -0,0 +1,18 @@
+
+const btn = document.querySelector('.cmp-languagenavigation')
+const nav = document.querySelector('.cmp-languagenavigation__group')
+
+
+btn.addEventListener('click', () => {
+    if (nav.style.display === 'none'){
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none'
+    }
+})
+
+
+/*
+var div = document.querySelector('cmp-languagenavigation');
+div.innerHTML += 'European Markets - English'; */
+
